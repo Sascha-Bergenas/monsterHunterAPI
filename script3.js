f66157b5252e41b9478bb9e1fc9c66e70478dd61
Baimako <sascha.bergenas@hotmail.com>
2025-10-25 21:24:59 +0200

    strukterat upp javascripten och gjort den mer "clean code"

--- script3.js
+++ script3.js
@@ -1,5 +1,52 @@
+const nameElement = document.getElementById("monsterName");
+const typeElement = document.getElementById("monsterType");
+const weaknessElement = document.getElementById("monsterWeaknesses");
+const resistancesElement = document.getElementById("monsterResistances");
+const typeHeading = document.getElementById("typeHeading");
+const weaknessesHeading = document.getElementById("weaknessesHeading");
+const resistancesHeading = document.getElementById("resistancesHeading");
+
+const displayLoading = () => {
+  nameElement.textContent = "Loading...";
+};
+
+const resetNameElement = () => {
+  nameElement.textContent = "";
+};
+
+const clearMonsterInformation = () => {
+  typeElement.textContent = "";
+  weaknessesHeading.textContent = "";
+  weaknessElement.textContent = "";
+  resistancesHeading.textContent = "";
+  resistancesElement.textContent = "";
+  typeHeading.textContent = "";
+};
+
+const displayWeaknesses = (monster) => {
+  const weaknesses =
+    monster.weaknesses && monster.weaknesses.length > 0
+      ? monster.weaknesses.map((w) => `${w.element} ${w.stars}★`).join(", ")
+      : "No weknesses";
+
+  weaknessesHeading.textContent = "Weaknesses:";
+  weaknessElement.textContent = `${weaknesses}`;
+};
+
+const displayResistances = (monster) => {
+  const resistances =
+    monster.resistances && monster.resistances.length > 0
+      ? monster.resistances.map((res) => `${res.element}`).join(", ")
+      : "No resistances";
+
+  resistancesHeading.textContent = `Resistances:`;
+  resistancesElement.textContent = `${resistances}`;
+};
+
 async function getMonsters() {
   try {
+    displayLoading();
+    clearMonsterInformation();
     const response = await fetch("https://mhw-db.com/monsters");
 
     if (!response.ok) {
@@ -9,27 +56,24 @@ async function getMonsters() {
     return data;
   } catch (error) {
     console.error("fel", error);
+  } finally {
+    resetNameElement();
   }
 }
-const nameElement = document.getElementById("monsterName");
-const typeElement = document.getElementById("monsterType");
-const weaknessElement = document.getElementById("monsterWeaknesses");
 
-async function showRndMonster() {
-  const monsters = await getMonsters();
-  if (monsters.length === 0) {
+async function showRandomMonster() {
+  const allMonsters = await getMonsters();
+  if (allMonsters.length === 0) {
     nameElement.textContent = "Not found";
   }
-  const randomIndex = Math.floor(Math.random() * monsters.length);
-  const monster = monsters[randomIndex];
-
-  const weaknesses =
-    monster.weaknesses && monster.weaknesses.length > 0
-      ? monster.weaknesses.map((w) => `${w.element} ${w.stars}★`).join(", ")
-      : "No weknesses";
+  const randomIndex = Math.floor(Math.random() * allMonsters.length);
+  const monster = allMonsters[randomIndex];
+  console.log(monster);
+  displayWeaknesses(monster);
+  displayResistances(monster);
 
   nameElement.textContent = monster.name;
-  typeElement.textContent = `Type: ${monster.type} (${monster.species})`;
-  weaknessElement.textContent = `Weaknesses: ${weaknesses}`;
+  typeHeading.textContent = "Type:";
+  typeElement.textContent = ` ${monster.type} (${monster.species})`;
 }
-document.getElementById("btn").addEventListener("click", showRndMonster);
+document.getElementById("btn").addEventListener("click", showRandomMonster);
